8c9b997340fe57a4877910e85108e82416c3f4d9
Mariola Rubio <mariola.rubio.canton@accenture.com>
2022-01-18 16:37:50 +0000

    New working checkWinner algorithm

--- connect4.js
+++ connect4.js
@@ -1,90 +1,168 @@
 // JS file for the Connect 4 game
 
+// TODO:
+// Improve UI
+// Show who's turn it is
+// Make functions pure
+// Ask Yingying about UI animation
+
 // gameState object
-let gameState = {
-    turn: 0,
-    player: "red",
-    board: [
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null]
-    ]
+const gameState = {
+  turn: 0,
+  player: 'red',
+  winner: null,
+  board: [
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null]
+  ]
 }
 
+// eslint-disable-next-line no-unused-vars
+function takeTurn (e) {
+  const id = e.target.id
+  //   const row = id[3] // don't think I need this
+  const column = id[8]
 
-// TODO: 
-// check max turn hasn't been reached
-// winner message cleared after reset
+  const lowestFreeRow = getLowestFreeRowInColumn(column, gameState.board)
+  // console.log(`LowestFree row: ${lowestFreeRow}`)
 
+  if (lowestFreeRow !== null && gameState.winner == null && gameState.turn < 43) {
+    gameState.turn++
 
-function takeTurn(e) {
-    const id = e.target.id
-    const row = id[3]
-    const column = id[8]
+    if (gameState.player === 'red') {
+      gameState.board[lowestFreeRow][column] = 'red'
+      document.getElementById(`row${lowestFreeRow}-col${column}`).style.backgroundColor = 'red'
+      gameState.player = 'yellow'
+    } else {
+      gameState.board[lowestFreeRow][column] = 'yellow'
+      document.getElementById(`row${lowestFreeRow}-col${column}`).style.backgroundColor = 'yellow'
+      gameState.player = 'red'
+    }
+  }
 
-    const lowestFreeRow = getLowestFreeRowInColumn(column, gameState.board)
-    console.log(`LowestFree row: ${lowestFreeRow}`)
+  gameState.winner = checkWinner()
+  console.log(gameState.winner)
+  if (gameState.winner != null) {
+    document.getElementById('winnerMessage').innerText = `The winner is ${gameState.winner}`
+  } else if (gameState.winner === null && gameState.turn === 42) {
+    document.getElementById('winnerMessage').innerText = 'The winner is nobody'
+  }
+}
 
-    if (lowestFreeRow !== null) {
-        gameState.turn++
+// function getElement (r, c) {
+//   const element = document.querySelector(`.row#${r} > .column#${c}`)
+// }
 
-        if (gameState.player === "red") {
-            gameState.board[lowestFreeRow][column] = "red"
-            document.getElementById(`row${lowestFreeRow}-col${column}`).style.backgroundColor = 'red';
-            gameState.player = "yellow"
-        } else {
-            gameState.board[lowestFreeRow][column] = "yellow"
-            document.getElementById(`row${lowestFreeRow}-col${column}`).style.backgroundColor = 'yellow';
-            gameState.player = "red"
+function getLowestFreeRowInColumn (colNumber, grid) {
+  for (let i = 5; i >= 0; i--) {
+    if (grid[i][colNumber] === null) {
+      return i
+    }
+  }
+
+  return null
+}
+
+// eslint-disable-next-line no-unused-vars
+function reset () {
+  gameState.turn = 0
+  gameState.player = 'red'
+  gameState.board = [
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null]
+  ]
+
+  const columns = document.getElementsByClassName('col')
+  for (let i = 0; i < columns.length; i++) {
+    columns[i].style.backgroundColor = 'transparent'
+    // columns[i].removeProperty("backgroundColor")
+  }
+
+  document.getElementById('winnerMessage').innerText = ''
+}
+
+function checkWinner () {
+  const rowWin = checkRows()
+  const columnWin = checkColumns()
+  const diagWin = checkDiagonals()
+  const counterDiagWin = checkCounterDiagonals()
+  if (rowWin != null) {
+    return rowWin
+  } else if (columnWin != null) {
+    return columnWin
+  } else if (diagWin != null) {
+    return diagWin
+  } else if (counterDiagWin != null) {
+    return counterDiagWin
+  } else {
+    return null
+  }
+}
+
+function checkRows () {
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (gameState.board[i][j] != null) {
+        if (gameState.board[i][j] === gameState.board[i][j + 1] &&
+                    gameState.board[i][j] === gameState.board[i][j + 2] &&
+                    gameState.board[i][j] === gameState.board[i][j + 3]) {
+          return gameState.board[i][j]
         }
+      }
     }
-
-    console.log(`You clicked column ${column}`)
-    console.log(`Turn number ${gameState.turn}`)
-    console.log(gameState.board)
+  }
+  return null
 }
 
-
-function getElement(r,c) {
-    const element = document.querySelector(`.row#${r} > .column#${c}`)
-
-}
-
-
-function getLowestFreeRowInColumn(colNumber, grid) {
-    for (let i = 5; i >= 0; i--) {
-        if (grid[i][colNumber] === null) {
-            return i
+function checkColumns () {
+  for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 6; j++) {
+      if (gameState.board[j][i] != null) {
+        if (gameState.board[j][i] === gameState.board[j - 1][i] &&
+                    gameState.board[j][i] === gameState.board[j - 2][i] &&
+                    gameState.board[j][i] === gameState.board[j - 3][i]) {
+          return gameState.board[j][i]
         }
+      }
     }
-
-    return null;
+  }
+  return null
 }
 
-
-function reset() {
-    gameState.turn = 0
-    gameState.player = "red"
-    gameState.board = [
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null]
-    ]
-
-    let columns = document.getElementsByClassName("col")
-    for (let i = 0; i < columns.length; i++) {
-        columns[i].style.backgroundColor = "transparent"
-        // columns[i].removeProperty("backgroundColor")
+function checkDiagonals () {
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (gameState.board[i][j] != null) {
+        if (gameState.board[i][j] === gameState.board[i - 1][j + 1] &&
+                      gameState.board[i][j] === gameState.board[i - 2][j + 2] &&
+                      gameState.board[i][j] === gameState.board[i - 3][j + 3]) {
+          return gameState.board[i][j]
+        }
+      }
     }
+  }
+  return null
 }
 
-
-function checkWinner() {
-
+function checkCounterDiagonals () {
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (gameState.board[i][j] != null) {
+        if (gameState.board[i][j] === gameState.board[i - 1][j - 1] &&
+                        gameState.board[i][j] === gameState.board[i - 2][j - 2] &&
+                        gameState.board[i][j] === gameState.board[i - 3][j - 3]) {
+          return gameState.board[i][j]
+        }
+      }
+    }
+  }
+  return null
 }
