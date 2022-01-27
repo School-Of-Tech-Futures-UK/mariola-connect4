e1b9b87f8e843e86ad466fb62f68fb8d1b0beade
Mariola Rubio <mariola.rubio.canton@accenture.com>
2022-01-27 12:16:00 +0000

    Removed console logs and moved impure API functions to Dirty Layer.

--- front-end/connect4.js
+++ front-end/connect4.js
@@ -1,10 +1,6 @@
 // Functions used by game-state.js to implement connect4
 
 // TODO:
-// Make functions pure
-//    Break down code into smaller functions
-//    Look for parts of the code that are repeated (those could be a separate function)
-// Add winning colour to state
 // Improve UI
 // Merge send and get scores
 
@@ -33,69 +29,16 @@ let gameState = {
 
 // takeTurn is called every time a user clicks on the board
 // eslint-disable-next-line no-unused-vars
-function takeTurn (e) {
-  gameState = makeMove(e, gameState)
+async function takeTurn (e) {
+  gameState = await makeMove(e, gameState)
   drawBoard(gameState)
 }
 
+// resetGame is called when a user clicks on the reset button
 // eslint-disable-next-line no-unused-vars
 function resetGame () {
   gameState = reset(gameState)
   clearBoard()
-}
-
-// ------------------------ PURE FUNCTIONS ------------------------
-
-function makeMove (e, state) {
-  const newState = JSON.parse(JSON.stringify(state))
-
-  const id = e.target.id
-  const column = id[8]
-  const lowestFreeRow = getLowestFreeRowInColumn(column, newState.board)
-
-  if (lowestFreeRow !== null && newState.winner == null && newState.turn < 43) {
-    newState.turn++
-
-    if (newState.player === 'Red') {
-      newState.board[lowestFreeRow][column] = 'Red'
-      newState.player = 'Yellow'
-    } else {
-      newState.board[lowestFreeRow][column] = 'Yellow'
-      newState.player = 'Red'
-    }
-  }
-
-  newState.winner = checkWinner(newState)
-  console.log(newState.winner)
-  if (newState.winner === null) {
-    displayTurn(newState.player)
-  } else {
-    newState.finalScore = 42 - newState.turn
-    if (newState.winner === 'Red') {
-      newState.redPlayerName = document.getElementById('red-input').value
-      newState.winningPlayer = newState.redPlayerName
-      newState.winningColour = 'Red'
-    } else if (newState.winner === 'Yellow') {
-      newState.yellowPlayerName = document.getElementById('yellow-input').value
-      newState.winningPlayer = newState.yellowPlayerName
-      newState.winningColour = 'Yellow'
-    }
-    console.log('final score: ', newState.finalScore)
-    sendScore(newState).then(async () => {
-      newState.highScores = await getHighScores()
-    })
-    console.log('high scores from makeMove(): ', newState.highScores)
-  }
-  return newState
-}
-
-function getLowestFreeRowInColumn (colNumber, grid) {
-  for (let i = 5; i >= 0; i--) {
-    if (grid[i][colNumber] === null) {
-      return i
-    }
-  }
-  return null
 }
 
 async function sendScore (state) {
@@ -115,8 +58,57 @@ async function sendScore (state) {
 async function getHighScores () {
   const req = await fetch('http://localhost:3000/scores')
   const scores = await req.json() // This is an array of {player: name, score: , colour: } objects
-  // console.log(scores)
   return scores
+}
+
+// ------------------------ PURE FUNCTIONS ------------------------
+
+async function makeMove (e, state) {
+  const newState = JSON.parse(JSON.stringify(state))
+
+  const id = e.target.id
+  const column = id[8]
+  const lowestFreeRow = getLowestFreeRowInColumn(column, newState.board)
+
+  if (lowestFreeRow !== null && newState.winner == null && newState.turn < 43) {
+    newState.turn++
+
+    if (newState.player === 'Red') {
+      newState.board[lowestFreeRow][column] = 'Red'
+      newState.player = 'Yellow'
+    } else {
+      newState.board[lowestFreeRow][column] = 'Yellow'
+      newState.player = 'Red'
+    }
+  }
+
+  newState.winner = checkWinner(newState)
+  if (newState.winner === null) {
+    displayTurn(newState.player)
+  } else {
+    newState.finalScore = 42 - newState.turn
+    if (newState.winner === 'Red') {
+      newState.redPlayerName = document.getElementById('red-input').value
+      newState.winningPlayer = newState.redPlayerName
+      newState.winningColour = 'Red'
+    } else if (newState.winner === 'Yellow') {
+      newState.yellowPlayerName = document.getElementById('yellow-input').value
+      newState.winningPlayer = newState.yellowPlayerName
+      newState.winningColour = 'Yellow'
+    }
+    await sendScore(newState)
+    newState.highScores = await getHighScores()
+  }
+  return newState
+}
+
+function getLowestFreeRowInColumn (colNumber, grid) {
+  for (let i = 5; i >= 0; i--) {
+    if (grid[i][colNumber] === null) {
+      return i
+    }
+  }
+  return null
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -141,6 +133,7 @@ function reset (state) {
   return newState
 }
 
+// PURE ✔️
 function checkWinner (state) {
   const rowWin = checkRows(state)
   const columnWin = checkColumns(state)
@@ -269,7 +262,6 @@ function displayWinner (state) {
 function displayScores (state) {
   document.getElementById('scoreboard').classList.remove('invisible')
   document.getElementById('scoreboard').classList.add('visible')
-  console.log('The high scores from displayScores() function', state.highScores)
   for (let i = 0; i < state.highScores.length; i++) {
     console.log('in scoreboard for loop')
     document.getElementById(`score${i + 1}`).innerText = state.highScores[i].player + ' (' + state.highScores[i].colour + '): ' + state.highScores[i].score
